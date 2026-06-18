b40fcd3587fe8c38a5e482512998d5349606e816
Badali <djisadpokksad@gmail.com>
2026-06-18 13:55:58 +0800

    Update ProductSeeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -10,24 +10,43 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         Product::create([
-            'name' => 'Laptop ASUS',
-            'description' => 'Laptop gaming performa tinggi',
-            'image' => 'laptop.jpg',
-            'price' => 12000000,
+            'id' => 1,
+            'name' => 'Kamera',
+            'description' => 'Best kamera',
+            'image' => 'kamera.png',
+            'price' => 9999,
         ]);
 
         Product::create([
-            'name' => 'Mouse Logitech',
-            'description' => 'Mouse wireless nyaman digunakan',
-            'image' => 'mouse.jpg',
-            'price' => 250000,
+            'id' => 2,
+            'name' => 'Lensa Kamera',
+            'description' => 'Best Lensa',
+            'image' => '2.png',
+            'price' => 999,
         ]);
 
         Product::create([
-            'name' => 'Keyboard Mechanical',
-            'description' => 'Keyboard mechanical RGB',
-            'image' => 'keyboard.jpg',
-            'price' => 750000,
+            'id' => 3,
+            'name' => 'Tas Kamera',
+            'description' => 'Best Tas Kamera',
+            'image' => 'taskamera.png',
+            'price' => 30,
+        ]);
+
+        Product::create([
+            'id' => 5,
+            'name' => 'Professional Camera',
+            'description' => 'Kamera Untuk Orang Jago',
+            'image' => '5.png',
+            'price' => 99999,
+        ]);
+
+        Product::create([
+            'id' => 6,
+            'name' => 'Lensa kamera terbaru',
+            'description' => 'Keluaran terbaru',
+            'image' => '6.png',
+            'price' => 15000,
         ]);
     }
 }
